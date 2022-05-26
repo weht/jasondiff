1c402b60a4a3266dd2a5ee5ce3bee7f8ee80ff44
jang guk han <88602381+weht@users.noreply.github.com>
2022-05-26 16:52:33 +0900

    #1 메인화면 UI 구성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div class="main-body">
+      <div class="body-top">
+        <div class="body-title">Json 입력 1</div>
+        <div class="body-title">Json 입력 2</div>
+      </div>
+      <div class="body-bottom">
+        <div class="main-json">
+          <textarea v-model="jsonData1" name="json1" id="json1" cols="80" rows="40" placeholder="비교할 json을 넣어주세요"></textarea>
+        </div>
+        <div class="main-json">
+          <textarea v-model="jsonData2" name="json2" id="json2" cols="80" rows="40" placeholder="비교할 json을 넣어주세요"></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-button"><button class="compare">compare now</button></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      jsonData1: '',
+      jsonDate2: '',
+    }
+  },
+  methods: {},
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 50px 50px;
+  padding-top: 50px;
+  width: 95%;
+  min-height: 85vh;
+  background: #edf0f7;
+  overflow-y: auto;
+}
+*::-webkit-scrollbar {
+  display: none;
+}
+
+.body-top {
+  width: 95%;
+  display: flex;
+  justify-content: space-around;
+  font-size: 30px;
+  font-weight: 600;
+}
+
+.body-bottom {
+  width: 95%;
+  height: 70vh;
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-around;
+}
+#json1,
+#json2 {
+  margin-top: 20px;
+  padding: 5px;
+  border-radius: 10px;
+}
+.bottom-button {
+  width: 95%;
+  display: flex;
+  justify-content: center;
+}
+.compare {
+  font-size: 20px;
+  cursor: pointer;
 }
 </style>
